6768f837bd4b681192e91ff7a3482c6ee37ea3d7
hanyaegbuna <helenedwin32@gmail.com>
2022-09-08 16:30:49 -0700

    file c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
 #include <stdio.h>
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-}
+int main(void) { puts("\"Programming is like building a multilingual puzzle");
+	return(0); }
