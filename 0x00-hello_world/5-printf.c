5a457f2a1c5e122d1e07d3924c70f8d5ee46a336
hanyaegbuna <helenedwin32@gmail.com>
2022-09-09 03:34:16 -0700

    file c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
+{
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+	return (0); }
 }
