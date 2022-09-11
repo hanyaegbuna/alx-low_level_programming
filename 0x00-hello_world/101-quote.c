c61682ee9494bada16f495af6f5dfa7928bd9730
hanyaegbuna <helenedwin32@gmail.com>
2022-09-11 10:53:32 -0700

    file c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,14 +5,16 @@
 
 
 /**
- * main - prints exactly "and that piece of art is useful"
+ * main - prints exactly
+ *"and that piece of art is useful"
  * Return: 1 if success
  */
 
 int main(void)
 
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art is useful\"
+			       	- Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
