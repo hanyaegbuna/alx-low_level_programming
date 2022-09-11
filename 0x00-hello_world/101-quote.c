9e01c932a13e9b63ee7256f211faa59b30d8bd77
hanyaegbuna <helenedwin32@gmail.com>
2022-09-11 10:42:25 -0700

    file c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,14 +6,12 @@
 
 /**
  * main - prints exactly "and that piece of art is useful"
- * followed by a new line, to the standard error.
  * Return: 1 if success
  */
 
 int main(void)
 
 {
-
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
