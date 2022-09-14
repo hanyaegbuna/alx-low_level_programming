50a79d2fb2ae48265054e7cf6092f5ab4168dabe
hanyaegbuna <helenedwin32@gmail.com>
2022-09-14 08:46:31 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+ * Main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
