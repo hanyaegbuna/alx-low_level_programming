84f018b339b86c9d1e0545e3b109a6d4217e6b2a
hanyaegbuna <helenedwin32@gmail.com>
2022-09-20 13:33:53 -0700

    c nested

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@ void print_rev(char *s)
 	int i, n;
 
 	n = 0;
-	while (s[n] != '\0');
+	while (s[n] != '\0')
 		n++;
 
-	for (i = n - 1, i >= 0; i--);
-	{
+	for (i = n - 1; i >= 0; i--)
+	
 		_putchar(s[i]);
 	}
 	_putchar('\n');
