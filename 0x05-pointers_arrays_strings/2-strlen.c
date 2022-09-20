f8dfdea4596b1e29ce65daedf258e2d22e1ff21b
hanyaegbuna <helenedwin32@gmail.com>
2022-09-20 05:12:41 -0700

    c nested

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -> function to get the lenght of a string
+ * @s: string pointer to passed to this function
+ * RETURN: returns length of the string
+ */
+
+int _strlen(char *s)
+{{
+	return (strlen(s));
+ }
