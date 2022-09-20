a548a9878bd87750be9effb37a75517f344dbf43
hanyaegbuna <helenedwin32@gmail.com>
2022-09-20 14:04:22 -0700

    c nested

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * reverse_array - Reverses the content of an array of integers
- * @a: The array of integers to be reversed.
- * @n: The number of elements in the array.
+ * print_rev -> printing a string in reverse
+ * @s: the string to be printed in rev
  */
 void print_rev(char *s)
 {
