cf710dd8ec0e830cf646d313caef4cc52a89c8a0
hanyaegbuna <helenedwin32@gmail.com>
2022-09-20 19:20:14 -0700

    c nested

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,11 +12,11 @@ void rev_string(char *s)
 	length1 = 0;
 	length2 = 0;
 
-	while(s[length1] != '\0')
+	while (s[length1] != '\0')
 		length1++;
 
 	length2 = length1 - 1;
-	for (i = 0; i < length1 /2; i++)
+	for (i = 0; i < length1 / 2; i++)
 	{
 		tmp = s[i];
 		s[i] = s[length2];
