0a5aa17e06a3de0bd192a41fcdb7eb06a4a8481e
hanyaegbuna <helenedwin32@gmail.com>
2022-09-20 19:38:36 -0700

    c nested

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * puts2 >- puts2 function
  * @str: str parameter
@@ -9,7 +10,7 @@ void puts2(char *str)
 	int len, i;
 
 	len = strlen(str);
-	for (i = 0; i < len; 1 += 2)
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
