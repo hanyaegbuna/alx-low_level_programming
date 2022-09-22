c39547945effd668fba8f665ed182d920036dfb3
hanyaegbuna <helenedwin32@gmail.com>
2022-09-22 15:24:43 -0700

    c nested

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * reverse_array -> reversing arrray
+ * @a: array a
+ * @n: an element of an array
+ *
+ * Return: empty
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
