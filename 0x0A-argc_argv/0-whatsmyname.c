5306bb73464ee92e910cf7df6ca46fea62805d89
hanyaegbuna <helenedwin32@gmail.com>
2022-09-30 07:09:50 -0700

    c nested

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char ** argv)
 {
-	char *appname;
-
-	appname = strrchr(argv[0], '/'); /* '\\' on Windows */
-	printf("%s\n", appname ? ++appname : argv[0]);
+	printf("%s", argv[0]);
 	return 0;
 }
