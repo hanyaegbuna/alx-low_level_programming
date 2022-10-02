e60eca7552e932d31112d97ae37d91c179555e91
hanyaegbuna <helenedwin32@gmail.com>
2022-10-01 21:08:47 -0700

    c nested

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * main - print the name of the program
- * @argc: count arguments
- * @argv: Arguments
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(int argc, char ** argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
