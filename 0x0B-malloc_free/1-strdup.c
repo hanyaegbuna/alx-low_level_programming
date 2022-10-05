f78400c77c450744b7768308a883df55b4c1ec64
hanyaegbuna <helenedwin32@gmail.com>
2022-10-05 07:10:48 -0700

    c nested

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - function that returns a pointer
+ * to a newly allocated space in memory, which contains
+ * a copy of the string given as a parameter.
+ *
+ * @str: char to initialize array with
+ * Return: a pointer to the duplicated string,
+ * or NULL if insufficient memory was available
+ */
+
+char *_strdup(char *str)
+{
+	char *dpstr;
+	int count;
+	unsigned int i;
+
+	if (str == NULL)
+
+		return (NULL);
+
+	for (count = 0; str[count] != '\0'; count++);
+	dpstr = malloc(sizeof(char) * count + 1);
+	if (dpstr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dpstr[i] = str[i];
+	}
+	dpstr[i + 1] = '\0';
+	return (dpstr);
+}
